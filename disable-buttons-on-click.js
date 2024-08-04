aad019c506e38cbd0ad2c2b61a850a2e9ca9a290
atensoftware <shailesh@atensoftware.com>
2024-08-04 15:21:56 -0400

    Create disable-buttons-on-click.js
    Initial commit

---
+++ disable-buttons-on-click.js
@@ -0,0 +1,77 @@
+
+// Define the wait image URL
+const DISABLE_BUTTONS_WAIT_IMAGE_URL = "https://cdnjs.cloudflare.com/ajax/libs/octicons/8.5.0/svg/clock.svg";
+
+// Attach event listeners to each button on the page that matches the selector
+function InitializeDisableButtons(selectors)
+{
+	if(selectors == null)
+	{
+		selectors = 'button.DisableOnClick';
+	}
+
+	// Get all buttons
+	var buttonNodeList = document.querySelectorAll(selectors);
+
+	// Loop through each button
+	buttonNodeList.forEach(buttonNode => {
+		// Do not attach a click handler to reset buttons.
+		// These can be disabled, but do not disable anything
+		if (buttonNode.tagName === 'BUTTON'
+			&& buttonNode.type === 'reset')
+		{
+			return;
+		}
+
+		// Add onclick event listener
+		buttonNode.addEventListener('click', function(event) {
+			DisableButtonsOnPage(selectors, event);
+		});
+	});
+
+	// Preload the background image to avoid rendering delay
+	if(buttonNodeList.length > 0)
+	{
+		// Create a new Image object
+		var img = new Image();
+
+		// Set the src attribute to the image URL
+		img.src = DISABLE_BUTTONS_WAIT_IMAGE_URL;
+	}
+}
+
+// Handle the button click by marking all the buttons matching the selector as disabled,
+//  and showing a wait icon on the clicked button.
+function DisableButtonsOnPage(selectors, event)
+{
+	// Get all buttons
+	var buttonNodeList = document.querySelectorAll(selectors, event);
+
+	// Loop through each button
+	buttonNodeList.forEach(buttonNode => {
+		// Disable the button
+		buttonNode.disabled = true;
+
+		// Set disabled class
+		buttonNode.classList.add("btn-disabled");
+
+		// Only the clicked button gets the wait icon
+		if (event.target == buttonNode)
+		{
+			// Wrap button contents in a span
+			buttonNode.innerHTML = '<span style="opacity: 0.4;">' + buttonNode.innerHTML + '</span>';
+
+			// Display the background spinner image
+			buttonNode.style.backgroundImage = "url('" + DISABLE_BUTTONS_WAIT_IMAGE_URL + "')";
+			buttonNode.style.backgroundSize = "auto 90%";
+			buttonNode.style.backgroundRepeat = "no-repeat";
+			buttonNode.style.backgroundPosition = "center";
+		}
+	});
+
+	// Since the submit buttons are disabled, they will no trigger a form submit
+	// So manually submit the form instead if it is the event target
+	if (event.target.tagName === 'BUTTON' && event.target.type === 'submit') {
+		event.target.form.submit();
+	}
+}
